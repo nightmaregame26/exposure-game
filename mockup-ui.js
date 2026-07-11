c9e82bf5b9ccec30836891cd5cdee617a3838d36
nightmaregame26 <blakeshields87@gmail.com>
2026-07-11 23:14:41 +1000

    Load direct panel asset UI v3

--- mockup-ui.js
+++ mockup-ui.js
@@ -2,7 +2,7 @@
   'use strict';
   if (document.querySelector('script[data-component-ui-loader]')) return;
   const script = document.createElement('script');
-  script.src = 'component-ui.js?v=components-2';
+  script.src = 'component-ui.js?v=components-3';
   script.defer = true;
   script.dataset.componentUiLoader = 'true';
   document.head.appendChild(script);
